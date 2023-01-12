d1d9c89db25f00690cd1546dd3890675158b3f4a
ankitamorey <ankitamore1963@gmail.com>
2023-01-12 16:30:21 +0530

    add the  addoperation

---
+++ src/com/collections/AddOperation.java
@@ -0,0 +1,12 @@
+package com.collections;
+
+public class AddOperation {
+	static int a=10;
+	static int b=14;
+	static int c =a+b;
+	public static void main(String[] args) {
+		System.out.println(c);
+		
+	}
+
+}
