008a79cd563fa975cfbd37b716119ec52897333e
ankitamorey <ankitamore1963@gmail.com>
2023-01-12 15:41:35 +0530

    add file for arraylist

---
+++ src/com/collections/ArrayListEx.java
@@ -0,0 +1,17 @@
+package com.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListEx {
+	public static void main(String[] args) {
+		List list=new ArrayList();
+		list.add("Ankita");
+		list.add("Ashu");
+		list.add("Soham");
+		list.add("Anu");
+		list.add("Tanush");
+		System.out.println(list);
+		
+	}
+}
---
+++ src/com/collections/LinkedListEx.java
@@ -0,0 +1,20 @@
+package com.collections;
+
+import java.util.LinkedList;
+
+public class LinkedListEx {
+	public static void main(String[] args) {
+		LinkedList<Integer> list=new LinkedList<Integer>();
+		list.add(2);
+		list.add(5);
+		list.add(7);
+		list.add(9);
+		for(Integer i:list) {
+			System.out.println(i);	
+			
+		}
+		
+		
+	}
+
+}
